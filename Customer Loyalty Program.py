c91690ec33951a2bdc7b069c4600784aaa093cec
Chansa Chishimba <chansazm@yahoo.co.uk>
2019-10-09 22:56:35 -0500

    Add files via upload

---
+++ Customer Loyalty Program.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 09:46:05 2019
+
+@author: chansa
+"""
+#1
+#The name of Customers in alphabetical order and their points
+for Customer in Customers.sort:
+    print (Customer)
+
+
+#2
+#Customers in three different categories
+
+
+# A dictionary with customers and their points
+Customers = {'Chansa':200,'Chan': 700,'Nosiku': 7000,'Walter':11000}
+
+
+#iterate on the dictionary to group customers according to their points
+for points in Customers:
+    if points >= 5000:
+        Platinum = points
+    elif points >= 1000:
+        Gold = points
+    elif points >= 0:
+        Silver = points
+
+
+#3 Name of customer with the highest points
+        for points in Customers:
+            return points.max()
+            print ("The name of the customer with highest point is:" Customer.maximum)
+        
+        
+#4 The average Loyalty points for all Customers
+        for points in customers:
+            return Customers.average()
+            print ("The name of the customer with highest point is:" max())
+
+
+
+        
+        
+        
